bc2370ff8f741dbe0c0d662be1142b7bc82e62c8
Herbert Gandia <hgandia@gmail.com>
2022-11-30 20:12:26 -0500

    4th Page Upside Parallax

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,59 +1,70 @@
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
-import cyberWeb from '../app/assets/cyberWeb.jpg';
-import heartOfTron from '../app/assets/heartOfTron.jpg';
+//import heartOfTron from '../app/assets/heartOfTron.jpg';
 
 const MainPage = () => {
 
     return(
-        <Parallax pages={4} style={{ top: '0', left: '0' }}>
+        <Parallax pages={5} style={{ top: '0', left: '0' }}>
             <ParallaxLayer
                 offset={0}
                 speed={2.5}
-                factor={2}
                 style={{ 
-                    color:'white',
+                    color:'lightblue',
                     fontSize: '55px',
                     display: 'flex', 
                     justifyContent: 'center', 
                     alignItems: 'center', 
-                    backgroundImage: `url(${cyberWeb})`,
-                    backgroundSize: 'cover'
+                    // backgroundImage: "linear-gradient(#B68FA2, #333B3E, #3E555E, #586F77, #ACBCB1)"
                 }}
             >
                 <p><strong>WELCOME TO THE BEGINNINGS OF MY PAGE</strong></p>
             </ParallaxLayer>
 
-            {/* <ParallaxLayer offset={1} speed={2} style={{ backgroundColor: 'steelblue' }} /> */}
+            <ParallaxLayer offset={1} speed={2} style={{ backgroundColor: '#ff6d6d' }} />
 
             <ParallaxLayer
                 offset={1}
+                factor={0.7}
                 speed={0.5}
-                factor={4}
                 style={{
                 display: 'flex',
                 justifyContent: 'center',
                 alignItems: 'center',
-                color: 'TEAL',
+                color: 'white',
                 fontSize: '55px',
-                backgroundImage: `url(${heartOfTron})`
+                // backgroundImage: `url(${heartOfTron})`
                 }}>
                 <p><strong>THIS IS THE SECOND PAGE</strong></p>
             </ParallaxLayer>   
 
             <ParallaxLayer
                 offset={2}
-                speed={1.5}
-                factor={4}
+                speed={-4}
+                factor={1}
                 style={{
                 display: 'flex',
                 justifyContent: 'center',
                 alignItems: 'center',
-                color: 'TEAL',
+                color: 'black',
                 fontSize: '55px',
                 backgroundColor: 'steelblue'
                 }}>
                 <p><strong>THIS IS THE THIRD PAGE</strong></p>
-            </ParallaxLayer>    
+            </ParallaxLayer> 
+            <ParallaxLayer 
+                offset={3} 
+                speed={2} 
+                style={{ 
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    fontSize: '55px',
+                    color: 'white', 
+                    backgroundColor: 'black' 
+                }}
+            >
+            <p><strong>THIS IS THE FOURTH PAGE</strong></p>
+            </ParallaxLayer>
         </Parallax>       
     );
 }
