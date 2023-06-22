32f51aef0963d3207252478c4159304b0948e71a
Herbert Gandia <hgandia@gmail.com>
2023-06-22 11:15:16 -0400

    Website Container Flow Finalized

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,9 +1,10 @@
 import { Nav, Navbar, NavLink, NavItem, NavbarToggler, Collapse } from 'reactstrap';
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 import Logo from '../app/assets/HG1.gif';
-import Slide1 from '../app/assets/Modern design/Slide1.JPG';
-import Slide2 from '../app/assets/Modern design/Slide2.JPG';
-import CircuitLine from '../app/assets/Slide1_Graphics.png';
+import Slide1 from '../app/assets/cyberWeb.jpg';
+import CircuitLine1 from '../app/assets/Slide1_Graphics.png';
+import CircuitLine2 from '../app/assets/Slide2_Graphics.png';
+import CircuitLine3 from '../app/assets/Slide3_Graphics.png';
 import styles from '../styles.module.css';
 import { Container, Row, Col } from 'reactstrap';
 import { useRef, useState } from 'react';
@@ -70,13 +71,27 @@ const MainPage = () => {
                                 </Nav>
                             </Collapse>
                         </Navbar>
-                    </ParallaxLayer>        
-                    <ParallaxLayer offset={1} speed={0.5}
-                        style={{ 
-                            backgroundColor: '#FFF2CC',  
+                    </ParallaxLayer> 
+                    <ParallaxLayer
+                        offset={1}
+                        factor={1}
+                        speed={0.5}
+                        style={{
+                            backgroundImage: `url(${Slide1})`,  
                             backgroundSize: 'cover',
-                            maxHeight: '20%',
-                            color:'darkred'
+                            backgroundAttachment: 'fixed',
+                            backgroundPosition: 'center',
+                            backgroundRepeat:'no-repeat',
+                            maxHeight: ''
+                        }} 
+                    />      
+                    <ParallaxLayer offset={1} speed={1.5}
+                        style={{
+                            backgroundImage: `url(${CircuitLine1})`,  
+                            backgroundSize: '5%',
+                            backgroundAttachment: 'fixed',
+                            backgroundPosition: 'right',
+                            backgroundRepeat:'no-repeat'
                         }}
                     >
                         <Navbar expand='md' light>
@@ -88,28 +103,28 @@ const MainPage = () => {
                             <Collapse isOpen={isOpen} navbar>
                                 <Nav className={styles.navigationFont} justified> 
                                     <NavItem className={styles.navigationFont} >
-                                        <NavLink style={{ color: '#000000'}}
+                                        <NavLink style={{ color: '#FFF2CC'}}
                                             onClick={() => ref.current.scrollTo(0)}
                                         >
                                             Home
                                         </NavLink>
                                     </NavItem>
                                     <NavItem className={styles.navigationFont}>
-                                        <NavLink style={{ color: '#000000'}}
+                                        <NavLink style={{ color: '#FFF2CC'}}
                                             onClick={() => ref.current.scrollTo(1)}
                                         >
                                             Projects
                                         </NavLink>
                                     </NavItem>
                                     <NavItem className={styles.navigationFont}>
-                                        <NavLink style={{ color: '#000000'}}
+                                        <NavLink style={{ color: '#FFF2CC'}}
                                             onClick={() => ref.current.scrollTo(2)}
                                         >
                                             About
                                         </NavLink>
                                     </NavItem>
                                     <NavItem className={styles.navigationFont}>
-                                        <NavLink style={{ color: '#000000'}}
+                                        <NavLink style={{ color: '#FFF2CC'}}
                                             onClick={() => ref.current.scrollTo(3)}
                                         >
                                             Contact
@@ -122,7 +137,8 @@ const MainPage = () => {
                             <Row>
                                 <Col style={{ 
                                     fontWeight: 'bold', 
-                                    marginTop: '445px'
+                                    marginTop: '440px',
+                                    color: '#FFF2CC'
                                 }}>
                                     
                                     <h2>This is a heading</h2>
@@ -147,6 +163,13 @@ const MainPage = () => {
                     <ParallaxLayer
                         offset={2}
                         factor={1}
+                        style={{
+                            backgroundImage: `url(${CircuitLine2})`,  
+                            backgroundSize: '9.2%',
+                            backgroundAttachment: 'fixed',
+                            backgroundPosition: 'right',
+                            backgroundRepeat:'no-repeat'
+                        }}
                     >
                         <Navbar expand='md' light>
                             <NavbarToggler 
@@ -157,28 +180,28 @@ const MainPage = () => {
                             <Collapse isOpen={isOpen} navbar>
                                 <Nav className={styles.navigationFont} justified> 
                                     <NavItem className={styles.navigationFont} >
-                                        <NavLink style={{ color: '#000000'}}
+                                        <NavLink style={{ color: '#FFF2CC'}}
                                             onClick={() => ref.current.scrollTo(0)}
                                         >
                                             Home
                                         </NavLink>
                                     </NavItem>
                                     <NavItem className={styles.navigationFont}>
-                                        <NavLink style={{ color: '#000000'}}
+                                        <NavLink style={{ color: '#FFF2CC'}}
                                             onClick={() => ref.current.scrollTo(1)}
                                         >
                                             Projects
                                         </NavLink>
                                     </NavItem>
                                     <NavItem className={styles.navigationFont}>
-                                        <NavLink style={{ color: '#000000'}}
+                                        <NavLink style={{ color: '#FFF2CC'}}
                                             onClick={() => ref.current.scrollTo(2)}
                                         >
                                             About
                                         </NavLink>
                                     </NavItem>
                                     <NavItem className={styles.navigationFont}>
-                                        <NavLink style={{ color: '#000000'}}
+                                        <NavLink style={{ color: '#FFF2CC'}}
                                             onClick={() => ref.current.scrollTo(3)}
                                         >
                                             Contact
@@ -218,7 +241,7 @@ const MainPage = () => {
                         factor={1} 
                         speed={-3.3}
                         style={{
-                            backgroundImage: `url(${CircuitLine})`,  
+                            backgroundImage: `url(${CircuitLine3})`,  
                             backgroundSize: '18%',
                             backgroundAttachment: 'fixed',
                             backgroundPosition: 'right',
